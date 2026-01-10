563ce68268918e48aa7295575b88c9bd7146c8d9
WMFM <streetstalkv1@gmail.com>
2026-01-10 10:23:16 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'purge-core'
+description 'Core Purge controller: blackout, sirens, reward multiplier, events.'
+author 'You & ChatGPT'
+version '1.0.0'
+
+shared_scripts {
+    
+}
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+dependency '/assetpacks'
